9364eb0adf563f42a3edb9e103ac2327fd515d47
anti-duhring <mateus.limavn@gmail.com>
2023-08-26 13:28:25 -0300

    feat: Created useMap hook

--- src/app/new-route/page.tsx
+++ src/app/new-route/page.tsx
@@ -1,9 +1,13 @@
 'use client'
 
-import type { FindPlaceFromTextResponseData } from "@googlemaps/google-maps-services-js"
-import { FormEvent } from "react"
+import type { DirectionsResponseData, FindPlaceFromTextResponseData } from "@googlemaps/google-maps-services-js"
+import { FormEvent, useRef } from "react"
+import { useMap } from "../hooks/useMap"
 
 export default function NewRoutePage() {
+    const mapContainerRef = useRef<HTMLDivElement>(null)
+    const map = useMap(mapContainerRef)
+
     const searchPlaces = async(e: FormEvent) => {
         e.preventDefault()
         const source = (document.querySelector('#source') as HTMLInputElement).value
@@ -28,21 +32,39 @@ export default function NewRoutePage() {
         const placeDestinationId = destinationPlace.candidates[0].place_id
 
         const directionsResponse = await fetch(`http://localhost:3000/directions?originId=${placeSourceId}&destinationId=${placeDestinationId}`)
-        const directions = await directionsResponse.json()
+        const directions: DirectionsResponseData & { request: any } = await directionsResponse.json()
         
-        console.log(directions) 
+        await map?.addRouteWithIcons({
+            routeId: '1',
+            startMarkerOptions: {
+                position: directions.routes[0].legs[0].start_location
+            },
+            endMarkerOptions: {
+                position: directions.routes[0].legs[0].end_location
+            },
+            carMarkerOptions: {
+                position: directions.routes[0].legs[0].start_location
+            }
+        })
     }
+
+
     return (
-        <div>
-            <h1>New route</h1>
-            <form 
-                style={{ display: 'flex', flexDirection: 'column'}}
-                onSubmit={searchPlaces}
-            >
-                <input id="source" name="source" type="text" placeholder="source" />
-                <input id="destination" name="destination" type="text" placeholder="destination" />
-                <button type="submit">Create</button>
-            </form>
+        <div style={{ display: 'flex', flexDirection: 'row', height: '100%', width: '100%'}}>
+            <aside>
+                <h1>New route</h1>
+                <form 
+                    style={{ display: 'flex', flexDirection: 'column'}}
+                    onSubmit={searchPlaces}
+                >
+                    <input id="source" name="source" type="text" placeholder="source" />
+                    <input id="destination" name="destination" type="text" placeholder="destination" />
+                    <button type="submit">Create</button>
+                </form>
+            </aside>
+            <main id="map" ref={mapContainerRef} style={{height: '100%', width: '100%'}}>
+
+            </main>
         </div>
     )
 }
